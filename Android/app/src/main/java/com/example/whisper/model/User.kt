d9aacb06ede25450175e6c6470090dde423bb951
mintie98 <thang3dinhat@gmail.com>
2024-05-31 11:32:37 +0900

    up : followInfo ok (Min)

---
+++ Android/app/src/main/java/com/example/whisper/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.whisper.model
+
+data class User(
+    val userId: String,
+    val userName: String,
+    val whisperCount: Int,
+    val followCount: Int,
+    val followerCount: Int
+)
